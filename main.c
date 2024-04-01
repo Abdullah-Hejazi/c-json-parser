d00b909bb8c2748ee34eea7a6434ddd925cf7438
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2024-04-01 20:45:54 +0200

    large json file testing

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    Pair* pair = parse_json("tests/data/test14.json");
+    Pair* pair = parse_json("tests/data/large-file.json");
 
     print_pair(pair, 0);
 
